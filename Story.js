2ca9fb2833b8ec7cd3b9d472b0cb6e4161546422
saloni1966 <salonisharma1966@gmail.com>
2021-05-14 12:21:35 +0530

    2nd part od story

--- Story.js
+++ Story.js
@@ -1,2 +1,3 @@
 Once upon a time there lived a Crocodile and a monkey,
 One day the crociodile was so hungry and resting below a tree.
+He saw the monkey eating berries, he asked the monkey for some berries as he was feeling hungry.CrocodileThe Monkey was kind Hearted.
